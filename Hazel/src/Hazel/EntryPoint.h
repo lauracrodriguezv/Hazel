e0b3b64a15f4a14c7c30676347413a5fdfb08200
lauracrodriguezv <85327912+lauracrodriguezv@users.noreply.github.com>
2024-04-26 12:14:30 -0500

    Logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,7 +7,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc , char** argv)
 {
-	printf("Hazel Engine \n");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialize Log!");
+
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
